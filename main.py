67bdeb815f0afffb2159c11bcf96d708fbf2b296
AryanDhanuka10 <a9936067905@gmail.com>
2025-02-23 17:42:33 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from src.cnnClassifier import logger # src.cnnClassifier is a local package and cnnClassifier can also be used
-logger.info("Welcome To CNNClassfier!")
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage  {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise(e)
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -6,13 +6,13 @@ import yaml
 from cnnClassifier import logger
 import json
 import joblib
-from ensure import ensure_annotation
+from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
 
-@ensure_annotation
+@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
@@ -27,7 +27,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yaml) as yml_file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
@@ -36,7 +36,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         raise e
 
-@ensure_annotation
+@ensure_annotations
 def create_directories(path_to_directories:list ,verbose=True):
     """ create list of directories
 
@@ -49,7 +49,7 @@ def create_directories(path_to_directories:list ,verbose=True):
     if verbose:
         logger.info("created directory at: {path}")
 
-@ensure_annotation
+@ensure_annotations
 def save_json(path:Path, data:dict):
     """ save json data
 
@@ -62,7 +62,7 @@ def save_json(path:Path, data:dict):
 
     logger.info("json file saved at: {path}")
 
-@ensure_annotation
+@ensure_annotations
 def load_json(path:Path) -> ConfigBox:
     """ load json files data
 
@@ -79,7 +79,7 @@ def load_json(path:Path) -> ConfigBox:
     logger.info(f"json file loaded successfuly from : {path}")
     return ConfigBox(content)
 
-@ensure_annotation
+@ensure_annotations
 def save_bin(data:Any , path:Path):
     """save binary file
     Args:
@@ -89,7 +89,7 @@ def save_bin(data:Any , path:Path):
     joblib.dump(value=data, filename=path)
     logger.info(f"Binary file saved at: {path}")
 
-@ensure_annotation
+@ensure_annotations
 def load_bin(path:Path) -> Any:
     """load binary data
     Args:
@@ -102,8 +102,8 @@ def load_bin(path:Path) -> Any:
     logger.info(f"binary file loaded from: {path}")
     return data
 
-@ensure_annotation
-def get_size():
+@ensure_annotations
+def get_size(path: Path) -> str:
     """get size in KB
     
     Args:
@@ -116,15 +116,15 @@ def get_size():
     return f"- {size_in_kb} KB"
 
 
-@ensure_annotation
+@ensure_annotations
 def decodeImage(imgstring, filename):
     imgdata = base64.b64decode(imgstring)
     with open(filename, "wb") as f:
         f.write(imgdata)
         f.close()
 
-    @ensure_annotation
-def encodeImage():
+@ensure_annotations
+def encodeImage(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
     
