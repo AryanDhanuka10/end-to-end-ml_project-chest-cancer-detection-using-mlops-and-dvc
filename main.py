be8ed229f7805f76df2609477519db989a15653b
AryanDhanuka10 <a9936067905@gmail.com>
2025-03-20 11:26:39 +0530

    Model Training

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    DataIngestion = DataIngestionTrainingPipeline()
+    DataIngestion.main()
     logger.info(f">>>>>>> stage  {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
 except Exception as e:
     logger.exception(e)
@@ -19,8 +20,21 @@ STAGE_NAME = "Prepare Base Model"
 try:
     logger.info(f"************************")
     logger.info(f">>>>>>>>> stage {STAGE_NAME}  started <<<<<<<<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
+    PrepareBaseModel = PrepareBaseModelTrainingPipeline()
+    PrepareBaseModel.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx===============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"************************")
+    logger.info(f">>>>>>>>> stage {STAGE_NAME}  started <<<<<<<<<<<")
+    Training = ModelTrainerPipeline()
+    Training.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx===============x")
 except Exception as e:
     logger.exception(e)
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -18,3 +18,15 @@ class PrepareBaseModelConfig:
     params_including_top : bool
     params_weights : str
     params_classes : int
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    validation_data: Path  # Added validation data path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
