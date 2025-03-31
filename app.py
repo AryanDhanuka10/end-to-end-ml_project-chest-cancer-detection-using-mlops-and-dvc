ec15c2727975523782ce4e3a8529f71fc4bc2642
AryanDhanuka10 <AryanDhanuka10@github.com>
2025-03-31 17:23:49 +0530

    Final Commits

--- app.py
+++ app.py
@@ -1,51 +1,44 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
+from flask import Flask , request , jsonify , render_template
 import os
-import numpy as np
+from flask_cors import CORS,cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 
-app = FastAPI()
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
-# Enable CORS (Cross-Origin Resource Sharing)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = Flask(__name__)
+CORS(app)
 
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
-        self.classifier = PredictionPipeline(self.filename)
+        self.classifier  = PredictionPipeline(self.filename)
 
-clApp = ClientApp()
-
-@app.get("/")
-def home():
-    return {"message": "Welcome to the FastAPI Lung Cancer Detection API"}
-
-@app.post("/train")
-def train():
-    os.system("dvc repro")  # Trigger model training
-    return {"message": "Training started successfully"}
-
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    # Save uploaded file
-    image_path = f"uploaded_{file.filename}"
-    with open(image_path, "wb") as f:
-        f.write(file.file.read())
-
-    # Run prediction
-    result = clApp.classifier.predict(image_path)
     
-    return {"prediction": result}
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    
+@app.route("/train", methods=['GET','POST'])
+@cross_origin()
+def trainRoute():
+    os.system("dvc repro")
+    # or  os.system("python main.py")
+    return "Training done successfully"
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    image = request.json['image']
+    decodeImage(image,clApp.filename)
+    result = clApp.classifier.predict()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    clApp = ClientApp()
+    # app.run(host='0.0.0.0', port=8080) # for local host
+    # app.run(host='0.0.0.0', port=8080) # for AWS
+    app.run(host='0.0.0.0', port=5000) # for AZURE
